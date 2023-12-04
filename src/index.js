c7a9342a3d16625079670a35cb85097d02df6d6c
mmdiq <Mafphp@gmail.com>
2023-12-04 14:40:38 +0330

    FIX

--- src/index.js
+++ src/index.js
@@ -7,5 +7,12 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+const apiCoreAddress = process.env.API_CORE_ADDRESS;
+if (apiCoreAddress) {
+    const script = document.createElement('script');
+    script.src = `${apiCoreAddress}/core/tracking/index.js`;
+    script.async = true;
+    document.head.appendChild(script);
+}
 
 registerServiceWorker();
